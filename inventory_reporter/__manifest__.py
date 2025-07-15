08e33499c146952d64e27d7ad47b1715c76c49b8
Erfan <erfanorom1386@gmail.com>
2025-07-15 14:55:42 +0430

    Update __manifest__.py

--- inventory_reporter/__manifest__.py
+++ inventory_reporter/__manifest__.py
@@ -1,15 +1,14 @@
 {
     "name": "Inventory Report",
-    "version": "1.0.0",
+    "version": "1.0.1",
     "depends": ["stock"],
-    "author": "mosiw",
+    "author": "Mosiw, Erfan",
     "category": "Inventory",
-    "summary": "go the hell",
+    "summary": "go to the hell",
     "description": "inventory reporter / pdf",
     "data": [
         "report/report_inventory_request_template.xml",
         "views/report_action.xml",
-        # "views/assets.xml",
     ],
     'assets': {
         'web.report_assets_common': [
